0e20464c531acab1208523e0da59d9df384f0250
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2023-02-16 23:02:09 +0900

    String 관련 Problem6 구현

---
+++ src/Problem6.java
@@ -0,0 +1,28 @@
+package src;
+
+import java.util.Scanner;
+
+public class Problem6 {
+    public String solution(String input) {
+        String result = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            String candidate = String.valueOf(input.charAt(i));
+            if(!result.contains(candidate)) {
+                result += candidate;
+            }
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Problem6 problem6 = new Problem6();
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        System.out.print(problem6.solution(input));
+    }
+}
