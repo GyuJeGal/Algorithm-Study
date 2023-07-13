c1d69bd56333149263122cbdc6807a6ce32786af
gyujegal <lkjdee0329@gmail.com>
2023-07-13 23:22:49 +0900

    feat : 백준 문제 1654 solve Key Point :

---
+++ BaekJoon/Problem1654.py
@@ -0,0 +1,21 @@
+import sys
+
+k, n = map(int, sys.stdin.readline().split())
+arr = []
+
+for i in range(k):
+    arr.append(int(input()))
+
+start = 1
+end = max(arr)
+
+while (start <= end):
+    mid = (start + end) // 2
+    cnt = 0
+    for i in range(k):
+        cnt += arr[i] // mid
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
