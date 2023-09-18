3af0fcce6c90ba5906bd9338c2121c204054c4d8
gyujegal <lkjdee0329@gmail.com>
2023-09-19 01:47:15 +0900

    feat : 백준 문제 1516 solve Key Point : 위상정렬 이용

--- BaekJoon/Problem1516.py
+++ BaekJoon/Problem1516.py
@@ -14,7 +14,7 @@ graph = [[] for _ in range(N + 1)]
 # 결과
 answer = [0] * (N + 1)
 
-# 연결관계 입력
+# 연결 관계 입력
 for i in range(1, N + 1):
     inputList = list(map(int, input().split()))
     for j in range(len(inputList)):
@@ -24,7 +24,7 @@ for i in range(1, N + 1):
             graph[inputList[j]].append(i)
             inDegree[i] += 1
 
-# 위상정렬을 위한 초기 세팅
+# 위상 정렬을 위한 초기 세팅
 que = collections.deque()
 for i in range(1, N + 1):
     if inDegree[i] == 0:
@@ -33,12 +33,19 @@ for i in range(1, N + 1):
 # 위상정렬 시작
 while que:
     v, t = que.popleft()
+    # 해당 건물 결과값 저장
     answer[v] = t + buildTime[v]
 
+    # 연결된 건물들 순회
     for i in graph[v]:
+        # 인접 차수 빼기
         inDegree[i] -= 1
+        # 어떤 건물을 짓기전 선행 건물이 완료된 최대 시간 저장
         answer[i] = max(answer[i], t + buildTime[v])
+
+        # 만약 인접 차수가 0인 경우 큐에 넣기
         if inDegree[i] == 0:
+            # 선행 건물이 완료된 최대 시간을 큐에 넣기
             que.append([i, max(t + buildTime[v], answer[i])])
 
 for i in range(1, N + 1):
