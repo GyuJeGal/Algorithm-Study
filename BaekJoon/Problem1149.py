702b7d0bbebbebdd9173832b2ef43f3ab9980348
gyujegal <lkjdee0329@gmail.com>
2023-10-06 23:44:22 +0900

    Feat : 백준 문제 1149 solve Key Point : DP 풀이

--- BaekJoon/Problem1149.py
+++ BaekJoon/Problem1149.py
@@ -6,7 +6,7 @@ n = int(input())
 for _ in range(n):
     data.append(list(map(int, input().split())))
 
-for i in range(1,n):
+for i in range(1, n):
     data[i][0] = min(data[i-1][1], data[i-1][2]) + data[i][0]
     data[i][1] = min(data[i-1][0], data[i-1][2]) + data[i][1]
     data[i][2] = min(data[i-1][0], data[i-1][1]) + data[i][2]
