68d89a996f67b4718541d1811f2c230b0fc6944b
gyujegal <lkjdee0329@gmail.com>
2023-10-14 23:49:25 +0900

    Refactor : Problem17298 solve KeyPoint :

--- Java-BaekJoon/Problems/src/Problem17298.java
+++ Java-BaekJoon/Problems/src/Problem17298.java
@@ -17,11 +17,11 @@ public class Problem17298 {
     static int[] solution(int N, int[] A) throws Exception {
         int[] answer = new int[N];
 
-        ArrayDeque<Main.Pair> que = new ArrayDeque<>();
+        ArrayDeque<Pair> que = new ArrayDeque<>();
 
         for (int i = 0; i < N; i++) {
             if (que.isEmpty()) {
-                que.addLast(new Main.Pair(A[i], i));
+                que.addLast(new Pair(A[i], i));
             }
             else {
                 while (true) {
@@ -30,21 +30,21 @@ public class Problem17298 {
                     }
                     if (que.getLast().value < A[i]) {
                         // que에서 뒤에 값 빼고 answer 갱신
-                        Main.Pair last = que.pollLast();
+                        Pair last = que.pollLast();
                         answer[last.index] = A[i];
                     }
                     else {
                         break;
                     }
                 }
-                que.addLast(new Main.Pair(A[i], i));
+                que.addLast(new Pair(A[i], i));
             }
         }
 
         int size = que.size();
 
         for (int i = 0; i < size; i++) {
-            Main.Pair last = que.pollLast();
+            Pair last = que.pollLast();
             answer[last.index] = -1;
         }
 
